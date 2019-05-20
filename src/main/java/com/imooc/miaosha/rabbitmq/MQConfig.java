4585cec76edaf979a339c5939ef3bac4bc279378
yylovell <yyforll@126.com>
2019-05-20 23:44:13 +0800

    init

--- src/main/java/com/imooc/miaosha/rabbitmq/MQConfig.java
+++ src/main/java/com/imooc/miaosha/rabbitmq/MQConfig.java
@@ -29,65 +29,65 @@ public class MQConfig {
 	 * */
 	@Bean
 	public Queue queue() {
-		return new Queue(QUEUE, true);
+		return new Queue(MIAOSHA_QUEUE, true);
 	}
 	
 	/**
 	 * Topic模式 交换机Exchange
 	 * */
-//	@Bean
-//	public Queue topicQueue1() {
-//		return new Queue(TOPIC_QUEUE1, true);
-//	}
-//	@Bean
-//	public Queue topicQueue2() {
-//		return new Queue(TOPIC_QUEUE2, true);
-//	}
-//	@Bean
-//	public TopicExchange topicExchage(){
-//		return new TopicExchange(TOPIC_EXCHANGE);
-//	}
-//	@Bean
-//	public Binding topicBinding1() {
-//		return BindingBuilder.bind(topicQueue1()).to(topicExchage()).with("topic.key1");
-//	}
-//	@Bean
-//	public Binding topicBinding2() {
-//		return BindingBuilder.bind(topicQueue2()).to(topicExchage()).with("topic.#");
-//	}
-//	/**
-//	 * Fanout模式 交换机Exchange
-//	 * */
-//	@Bean
-//	public FanoutExchange fanoutExchage(){
-//		return new FanoutExchange(FANOUT_EXCHANGE);
-//	}
-//	@Bean
-//	public Binding FanoutBinding1() {
-//		return BindingBuilder.bind(topicQueue1()).to(fanoutExchage());
-//	}
-//	@Bean
-//	public Binding FanoutBinding2() {
-//		return BindingBuilder.bind(topicQueue2()).to(fanoutExchage());
-//	}
-//	/**
-//	 * Header模式 交换机Exchange
-//	 * */
-//	@Bean
-//	public HeadersExchange headersExchage(){
-//		return new HeadersExchange(HEADERS_EXCHANGE);
-//	}
-//	@Bean
-//	public Queue headerQueue1() {
-//		return new Queue(HEADER_QUEUE, true);
-//	}
-//	@Bean
-//	public Binding headerBinding() {
-//		Map<String, Object> map = new HashMap<String, Object>();
-//		map.put("header1", "value1");
-//		map.put("header2", "value2");
-//		return BindingBuilder.bind(headerQueue1()).to(headersExchage()).whereAll(map).match();
-//	}
+	@Bean
+	public Queue topicQueue1() {
+		return new Queue(TOPIC_QUEUE1, true);
+	}
+	@Bean
+	public Queue topicQueue2() {
+		return new Queue(TOPIC_QUEUE2, true);
+	}
+	@Bean
+	public TopicExchange topicExchage(){
+		return new TopicExchange(TOPIC_EXCHANGE);
+	}
+	@Bean
+	public Binding topicBinding1() {
+		return BindingBuilder.bind(topicQueue1()).to(topicExchage()).with("topic.key1");
+	}
+	@Bean
+	public Binding topicBinding2() {
+		return BindingBuilder.bind(topicQueue2()).to(topicExchage()).with("topic.#");
+	}
+	/**
+	 * Fanout模式 交换机Exchange
+	 * */
+	@Bean
+	public FanoutExchange fanoutExchage(){
+		return new FanoutExchange(FANOUT_EXCHANGE);
+	}
+	@Bean
+	public Binding FanoutBinding1() {
+		return BindingBuilder.bind(topicQueue1()).to(fanoutExchage());
+	}
+	@Bean
+	public Binding FanoutBinding2() {
+		return BindingBuilder.bind(topicQueue2()).to(fanoutExchage());
+	}
+	/**
+	 * Header模式 交换机Exchange
+	 * */
+	@Bean
+	public HeadersExchange headersExchage(){
+		return new HeadersExchange(HEADERS_EXCHANGE);
+	}
+	@Bean
+	public Queue headerQueue1() {
+		return new Queue(HEADER_QUEUE, true);
+	}
+	@Bean
+	public Binding headerBinding() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("header1", "value1");
+		map.put("header2", "value2");
+		return BindingBuilder.bind(headerQueue1()).to(headersExchage()).whereAll(map).match();
+	}
 	
 	
 }
